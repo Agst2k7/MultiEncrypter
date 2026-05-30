2b43d4200e4837155bec7bb369609b748f86e262
Agst2k7 <carinhaprabaixo@gmail.com>
2026-05-30 01:04:04 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,11 +2,65 @@
 
 using namespace std;
 
-int main() {
+enum Page {
+    MENU,
+    XOR,
+    CAESAR,
+    EXIT
+};
 
-    int money;
+int main(int argc, char** argv) {
 
-    cin >> money*100;
+    if(argc < 2) {
+        Page now;
+        int int_input;
+        now = MENU;
+
+        while (now != EXIT) {
+    
+            switch (now) {
+    
+                case MENU: {
+                    cout << "MultiEncrypter by Agst" << endl;
+                    cout << "[1]-XOR" << endl;
+                    cout << "[2]-CAESAR" << endl;
+                    cout << "[3]-EXIT" << endl;
+
+                    cin >> int_input;
+
+                    switch(int_input) {
+
+                        case 1: { now = XOR; break; }
+
+                        case 2: { now = CAESAR; break; }
+
+                        case 3: { now = EXIT; break; }
+
+                        default: { cout << "Invalid option" << endl; break; }
+                    }
+
+                    break;
+                }
+
+
+                case XOR: {
+                    cout << "XOR MENU" << endl;
+                    now = EXIT;
+                    break;
+                }
+
+
+                case CAESAR: {
+                    cout << "CAESAR MENU" << endl;
+                    now = EXIT;
+                    break;
+                }
+    
+            }
+        }
+    }
+
+
 
     return 0;
 }
